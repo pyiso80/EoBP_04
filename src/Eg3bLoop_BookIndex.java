36412987daf8b50b4611e71762ea9a8fa70747db
pyisoe <pyi.so.80@gmail.com>
2018-12-19 13:36:32 +0630

    initial commit

--- src/Eg3bLoop_BookIndex.java
+++ src/Eg3bLoop_BookIndex.java
@@ -5,9 +5,12 @@ Book Index Printer
  */
 public class Eg3bLoop_BookIndex extends ConsoleProgram {
    public void run() {
+      //readLine("message") is used to read text input
       String firstWord = readLine("Enter 1st word: ");
       String pageNo = readLine("Enter page no: ");
       int totLineLength = 30;
+      //for String variable, the length of the string can be
+      //obtained using `varName.length()`
       int numberOfDots = totLineLength - (firstWord.length() + pageNo.length());
       print(firstWord);
       for (int i = 0; i < numberOfDots; i++) {
